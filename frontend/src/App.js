ad74b4045b7a153cfed55e892a445533a2474b5f
Paul <paul.arquier@edu.ece.fr>
2021-10-26 20:45:42 +0200

    Affichage d'un premier classement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,40 +3,34 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  state = {
-    post: [{}]
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: [],
+    };
   }
 
   componentDidMount() {
-    fetch('https://data.football-api.com/v3/standings/1221?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b')
-      //fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        return response.json()
-      })
+    fetch("https://data.football-api.com/v3/standings/1221?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b")
+      //fetch('https://www.scorebat.com/video-api/v1/')
+      .then((response) => response.json())
       .then((result) => {
         this.setState({ post: result })
-        console.log(result)
-        for (let i = 0; i < 20; i++) {
-          console.log(result[i].team_name)
-          console.log(result[i].points)
-
-
-        }
-
+        console.log(result);
       })
 
   }
 
   render() {
     return (
-      <div className="App">
-        <h1>Test</h1>
-        <div>
-
-
-          {this.state.post[0].team_name}
-        </div>
+      <div>
+        {this.state.post.map((post) => (
+          <div>
+            <p>{post.team_name} : {post.points}</p>
+          </div>
+        ))}
       </div>
+
     )
   }
 }
