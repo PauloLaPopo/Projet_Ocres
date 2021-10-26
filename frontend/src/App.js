901763a9d9cae9224adf010560b114dd23525f58
Paul <paul.arquier@edu.ece.fr>
2021-10-26 11:32:29 +0200

    Connexion API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    post: {}
+  }
+
+  componentDidMount() {
+    fetch('https://data.football-api.com/v3/competitions/1221?Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b')
+      //fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        return response.json()
+      })
+      .then((result) => {
+        this.setState({ post: result })
+        console.log(result)
+        console.log(result.name)
+      })
+    console.log(this.state.post.name)
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Test</h1>
+        {this.state.post.name}
+      </div>
+    )
+  }
 }
 
 export default App;
