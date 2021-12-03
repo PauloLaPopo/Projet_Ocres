9c7304cc2566c075caaf13ee6404f725e49b315c
Pouillard <antoinepouillard@hotmail.fr>
2021-12-04 00:12:16 +0100

    Ajout visuel widget live

--- frontend/src/Components/Widget6.js
+++ frontend/src/Components/Widget6.js
@@ -14,16 +14,35 @@ class Widget6 extends React.Component {
         };
     }
 
+    componentDidMount() {
+        fetch("http://localhost:3001/readMatch")
+            .then((response) => response.json())
+            .then((result) => {
+                this.setState({ post: result })
+                console.log(result);
+            })
+
+    }
+
 
     render() {
 
         return (
 
             <div class="background_live">
-                <div class="match_direct">Match en direct</div>
-                <div class="logo_score"><span class="logo_dom"><Logo team_name="Marseille" widht="70px" height="70px" /></span>    <span class="score_live">2 - 0</span> <span><Logo team_name="PSG" widht="70px" height="70px" /></span></div>
-                <div class="live"><span class="logo_live">Live</span></div>
-                <div class="infos"><Logo team_name="horloge" widht="15px" height="15px" /> 34" <Logo team_name="position" widht="15px" height="15px" /> Stade Vélodrome</div>
+                {this.state.post.slice(0, 1).map((post) => (
+                    <>
+                        <div class="match_direct">Match en direct</div>
+                        <div class="logo_score"><span class="logo_dom"><Logo team_name={post.Team1} widht="70px" height="70px" /></span>
+                            <span class="score_live">{post.ScoreTeam1} - {post.ScoreTeam2}</span>
+                            <span class="logo_dom"><Logo team_name={post.Team2} widht="70px" height="70px" /></span></div>
+                        <div class="live"><span class="logo_live">Live</span></div>
+                        <div class="infos"><Logo team_name="horloge" widht="15px" height="15px" /> 34"
+                            <Logo team_name="position" widht="15px" height="15px" /> {post.Place}</div>
+                    </>
+
+
+                ))}
             </div>
 
         )
